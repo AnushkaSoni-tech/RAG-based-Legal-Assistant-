7b12ba4e468c33dcd9848c366f6aceb3f9df13db
AnushkaSoni-tech <anushka.s812006@gmail.com>
2026-09-17 16:04:23 +0530

    Refactor ask_question function for better structure
    Refactor question input handling into a function and improve chat history management.

--- pipeline.py
+++ pipeline.py
@@ -23,55 +23,44 @@ if not api_key:
 
 
 client=genai.Client(api_key=api_key)
-chat_history=[]
-while True:
-    question=input("Enter a question: ")
-    if question.lower() in["quit","exit","bye"]:
-        break
-    retrieved_documents =hybrid_retrival(embedding_model,chunks,question,k=4)
-    # Create numbered context
+
+def ask_question(question, chat_history=None):
+
+    if chat_history is None:
+        chat_history = []
+
+    retrieved_documents = hybrid_retrival(
+        embedding_model,
+        chunks,
+        question,
+        k=4
+    )
+
     retrieved_context = "\n\n".join(
         [
             f"Source {i+1}:\n{doc.page_content}"
             for i, doc in enumerate(retrieved_documents)
         ]
     )
-    #conversation history
-    history="\n".join(
+
+    history = "\n".join(
         [
             f"{message['role']}:{message['content']}"
             for message in chat_history
         ]
     )
-    #prompt
-    formatted_prompt=prompt.format(
+
+    formatted_prompt = prompt.format(
         history=history,
         retrieved_context=retrieved_context,
         question=question
     )
-    print("🔎 Searching relevant legal provisions...")
 
     response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents=formatted_prompt
+        model="gemini-2.5-flash",
+        contents=formatted_prompt
     )
-    answer=response.text
-    
-    print("\nAI:", answer)
 
-    #save conversation
-    chat_history.append(
-        {
-            "role": "User",
-            "content": question
-        }
-    )
-    chat_history.append(
-        {
-            "role": "AI",
-            "content": answer
-        }
-    )
-    
+    answer = response.text
 
-
+    return answer, retrieved_documents
